3e75f5dfa6e627765b3292ad1a14fd230ec5a948
Cheering <13372680538@163.com>
2026-09-04 18:05:55 +0800

    Add GitHub link and fix application icons

--- src/MCModPlus/Views/SettingsPage.xaml.cs
+++ src/MCModPlus/Views/SettingsPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -17,6 +18,21 @@ public partial class SettingsPage : Page
         InitializeComponent();
     }
 
+    private void OpenGitHub_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/Cheering571/MCModPlus",
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"无法打开 GitHub 链接：{ex.Message}", "打开失败", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
     private void OnPagePreviewMouseDown(object sender, MouseButtonEventArgs e)
     {
         if (FindCurseForgeAction(e.OriginalSource as DependencyObject) is null)
